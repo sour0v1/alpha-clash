90bdc46e197c10dadbb16cbb3d728d31062dd36f
Sourov <sourovdas730@gmail.com>
2024-02-16 00:45:09 +0600

    again start and complete first part

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,24 +1,33 @@
-// home section
-
-// for pressing 'enter'
-document.addEventListener('keyup', displayPlaygroundSection);
-
-function displayPlaygroundSection(event) {
-    const getEnterKey = event.key;
-    const getPlaygroundSection = document.getElementById('playground-section');
+/* // hide and show section
+// method 1
+function play(){
+    // step 1 - hide the home section to add 'hidden' class to home section
     const getHomeSection = document.getElementById('home-section');
-    if (getEnterKey === 'Enter') {
-        getPlaygroundSection.classList.remove('hidden');
-        getHomeSection.classList.add('hidden');
-    }
-} 
-// for click the 'Play Now' button
-const getHomePlayBtn = document.getElementById('home-btn');
-getHomePlayBtn.addEventListener('click', displayPlaygroundSectionByClick);
+    getHomeSection.classList.add('hidden');
 
-function displayPlaygroundSectionByClick(){
+    // step 2 - show the playground section to remove the 'hidden' class
     const getPlaygroundSection = document.getElementById('playground-section');
-    const getHomeSection = document.getElementById('home-section');
     getPlaygroundSection.classList.remove('hidden');
-        getHomeSection.classList.add('hidden');
+    
+} */
+
+function continueGame() {
+    // step 1 - generate random alphabet
+    const alphabet = getRandomAlphabet();
+
+    // step 2 - set random alphabet to the screen
+    const getCurrentAlphabet = document.getElementById('display');
+    getCurrentAlphabet.innerText = alphabet;
+
+    // set background color to key
+    setBackgroundColor(alphabet);
+
+}
+
+// method 2
+function enterGame() {
+    hideElementById('home-section');
+    showElementById('playground-section');
+    continueGame();
+
 }
---
+++ script/utility.js
@@ -0,0 +1,29 @@
+// hide element by function
+function hideElementById(elmentId) {
+    const getElement = document.getElementById(elmentId);
+    getElement.classList.add('hidden');
+}
+// show element by function
+function showElementById(elementId) {
+    const getElement = document.getElementById(elementId);
+    getElement.classList.remove('hidden');
+}
+
+// create background color to key by function
+function setBackgroundColor(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+// create random alphabet
+function getRandomAlphabet() {
+    // create array of alphabet
+    const stringAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const arrayOfAlphabet = stringAlphabet.split('');
+    // create random index between 0 - 25
+    const randomNumber = Math.random() * 25;
+    const indexNumber = Math.round(randomNumber);
+
+    const randomAlphabet = arrayOfAlphabet[indexNumber];
+    return randomAlphabet;
+}
